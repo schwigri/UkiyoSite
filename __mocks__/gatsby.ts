688175e28e1fe1e7f5e2ea98f8a0b9ed630dfc83
Griffen Schwiesow <18409205+schwigri@users.noreply.github.com>
2022-07-08 00:25:03 -0700

    feat: introduce unit testing

---
+++ __mocks__/gatsby.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+const gatsby = jest.requireActual("gatsby");
+
+export default {
+	...gatsby,
+	Link: jest.fn().mockImplementation(({
+		activeClassName,
+		activeStyle,
+		getProps,
+		innerRef,
+		partiallyActive,
+		ref,
+		replace,
+		to,
+		...rest
+										}) => React.createElement("a", { ...rest, href: to })),
+	StaticQuery: jest.fn(),
+	graphql: jest.fn(),
+	useStaticQuery: jest.fn(),
+};
