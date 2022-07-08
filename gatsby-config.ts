ee510c0723ca2e0b4850d9f6665679f31debb620
Griffen Schwiesow <18409205+schwigri@users.noreply.github.com>
2022-07-07 23:30:19 -0700

    feat: initialize gatsby site

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+	siteMetadata: {
+		title: "Griffen Schwiesow",
+	},
+	graphqlTypegen: true,
+	plugins: [],
+};
+
+export default config;
