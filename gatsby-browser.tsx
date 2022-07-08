0db528400eb49026700e1bf0f1442cc098f82cfb
Griffen Schwiesow <18409205+schwigri@users.noreply.github.com>
2022-07-08 09:28:16 -0700

    feat: add code style checks

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,5 @@
 import * as layout from "./src/utils/layout";
 import type { GatsbyBrowser } from "gatsby";
 
-export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = layout.wrapPageElement;
+export const wrapPageElement: GatsbyBrowser["wrapPageElement"] =
+	layout.wrapPageElement;
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,4 +1,5 @@
-import * as layout  from "./src/utils/layout";
+import * as layout from "./src/utils/layout";
 import type { GatsbySSR } from "gatsby";
 
-export const wrapPageElement: GatsbySSR["wrapPageElement"] = layout.wrapPageElement;
+export const wrapPageElement: GatsbySSR["wrapPageElement"] =
+	layout.wrapPageElement;
