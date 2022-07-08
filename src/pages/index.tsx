a236543b9f91eb81ef0c87e477c2d269c4c4c7fe
Griffen Schwiesow <18409205+schwigri@users.noreply.github.com>
2022-07-07 23:30:19 -0700

    feat: initialize gatsby site

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import type { PageProps } from "gatsby";
+
+class IndexPage extends React.Component<PageProps> {
+	render(): React.ReactNode {
+		return <span>Index page</span>;
+	}
+}
+
+export default IndexPage;
