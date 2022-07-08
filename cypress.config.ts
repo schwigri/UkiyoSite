f7dda3ade2f8156beecdd9309c1ec9d93d37916b
Griffen Schwiesow <18409205+schwigri@users.noreply.github.com>
2022-07-08 01:34:22 -0700

    feat(ci): add cypress workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
 import { defineConfig } from "cypress";
+
+dotenv.config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
 
 const config = defineConfig({
 	e2e: {
-		baseUrl: "http://localhost:8000",
+		baseUrl: process.env.E2E_BASE_URL || "http://localhost:9000",
 	},
 });
 
